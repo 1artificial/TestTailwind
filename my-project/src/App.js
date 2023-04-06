dc8533a322f18b57a2773d9b1802be224c1ad711
1artificial <upliftthebusiness@gmail.com>
2023-04-06 21:56:22 +0300

    init

---
+++ my-project/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Main from "./Main";
+import "./index.css";
+import "./Navbar.js";
+import "./Main.js";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
