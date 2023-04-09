99023324fc3551a28b8069f01e70a6083beece82
1artificial <upliftthebusiness@gmail.com>
2023-04-09 11:44:48 +0300

    DeployAuth0AndFirebase

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,7 +1,10 @@
+// App.js
+import React from "react";
 import Navbar from "./Navbar";
 import Main from "./Main";
 import Login from "./Login";
 import Logout from "./Logout";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./index.css";
 import "./Navbar.js";
 import "./Main.js";
@@ -9,12 +12,18 @@ import "./Login.js";
 import "./Logout";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <>
       <Logout />
       <Login />
       <Navbar />
-      <Main />
+      {isAuthenticated && (
+        <>
+          <Main />
+        </>
+      )}
     </>
   );
 }
--- my-project/src/Login.js
+++ my-project/src/Login.js
@@ -1,3 +1,4 @@
+// Login.js
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -6,7 +7,10 @@ const Login = () => {
 
   return (
     !isAuthenticated && (
-      <div className="grid place-items-center h-screen">
+      <div
+        className="flex items-center justify-center min-h-screen"
+        style={{ backgroundColor: "rgba(0,0,0,0.1)" }}
+      >
         <button
           type="button"
           className="h-12 px-6 m-2 text-lg  text-white bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-orange-300 dark:focus:ring-orange-800 shadow-lg shadow-orange-500/50 dark:shadow-lg dark:shadow-orange-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
